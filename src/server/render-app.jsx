2bd4cf320dda71179dff052ace3d8cd25d71fd96
Nancy <nancy.sawaya@postlight.com>
2017-08-09 12:02:21 +0300

    feat: add blanket

--- src/server/render-app.jsx
+++ src/server/render-app.jsx
@@ -33,13 +33,14 @@ const renderApp = (location: string, plainPartialState: ?Object, routerContext: 
         ${head.meta}
         <link rel="stylesheet" href="${STATIC_PATH}/css/bootstrap.min.css">
         <style class="${JSS_SSR_CLASS}">${sheets.toString()}</style>
+        <script src="blanket.js"></script>
       </head>
       <body>
         <div class="${APP_CONTAINER_CLASS}">${appHtml}</div>
         <script>
           window.__PRELOADED_STATE__ = ${JSON.stringify(store.getState())}
         </script>
-        <script src="${isProd ? STATIC_PATH : `http://localhost:${WDS_PORT}/dist`}/js/bundle.js"></script>
+        <script src="${isProd ? STATIC_PATH : `http://localhost:${WDS_PORT}/dist`}/js/bundle.js" data-cover></script>
       </body>
     </html>`
   )
