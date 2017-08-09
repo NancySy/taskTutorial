65f702bf46ddc8e1a56644f814dae6f84ccf0f4f
Nancy <nancy.sawaya@postlight.com>
2017-08-09 13:30:42 +0300

    chore: add jest config

--- src/server/render-app.jsx
+++ src/server/render-app.jsx
@@ -12,8 +12,6 @@ import App from './../shared/app'
 import { APP_CONTAINER_CLASS, JSS_SSR_CLASS, STATIC_PATH, WDS_PORT } from '../shared/config'
 import { isProd } from '../shared/util'
 
-require('blanket')
-
 const renderApp = (location: string, plainPartialState: ?Object, routerContext: ?Object = {}) => {
   const store = initStore(plainPartialState)
   const sheets = new SheetsRegistry()
@@ -35,14 +33,13 @@ const renderApp = (location: string, plainPartialState: ?Object, routerContext: 
         ${head.meta}
         <link rel="stylesheet" href="${STATIC_PATH}/css/bootstrap.min.css">
         <style class="${JSS_SSR_CLASS}">${sheets.toString()}</style>
-        <script src="blanket.js" data-cover></script>
       </head>
       <body>
         <div class="${APP_CONTAINER_CLASS}">${appHtml}</div>
         <script>
           window.__PRELOADED_STATE__ = ${JSON.stringify(store.getState())}
         </script>
-        <script src="${isProd ? STATIC_PATH : `http://localhost:${WDS_PORT}/dist`}/js/bundle.js" data-cover></script>
+        <script src="${isProd ? STATIC_PATH : `http://localhost:${WDS_PORT}/dist`}/js/bundle.js"></script>
       </body>
     </html>`
   )
